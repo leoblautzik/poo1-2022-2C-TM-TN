4dac71a683de06f933edad3d18b9eccaee12477e
Leonardo Blautzik <leoblautzik@gmail.com>
2022-09-16 21:02:54 -0300

    Empresa Turno Noche + Cuentas Bancarias

--- Empresa/src/empresa/EmpleadoDePlantaTemporaria.java
+++ Empresa/src/empresa/EmpleadoDePlantaTemporaria.java
@@ -1,5 +1,23 @@
 package empresa;
 
 public class EmpleadoDePlantaTemporaria extends Empleado {
+	
+	private static final double VALOR_HORA = 200;
+	
+	
+	public EmpleadoDePlantaTemporaria(boolean casado, int hijos, int horasTrabajadas) {
+		super(casado, hijos, horasTrabajadas);
+		
+	}
 
+	@Override
+	public double getSalario() {
+		return  this.getHorasTrabajadas() * EmpleadoDePlantaTemporaria.getValorHora();
+		
+	}
+
+	public static double getValorHora() {
+		return VALOR_HORA;
+	}
+	
 }
